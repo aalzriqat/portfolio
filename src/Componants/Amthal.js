2836b0c8947028db08d81ca868a6a84322d5c63c
aalzriqat <aalzriqat@gmail.com>
2024-09-29 17:10:13 +0300

    Eliminated ServerSide

--- src/Componants/Amthal.js
+++ src/Componants/Amthal.js
@@ -1,39 +1,44 @@
-import React, { Component } from "react";
-import { serverUrl } from "../utils";
-class Amthal extends Component {
-  constructor() {
-    super();
-    this.state = { mathal: {},amthal:[] };
-    this.fetchAmthal=this.fetchAmthal.bind(this);
+import React, { useState, useEffect } from "react";
+import AMTHAL from "../data/Amthal.json";
+
+const getRandomIndex = () => Math.floor(Math.random() * AMTHAL.length);
+
+const randomMathal = () => AMTHAL[getRandomIndex()];
+
+const randomTen = () => {
+  const randomList = new Set();
+  while (randomList.size < 10) {
+    randomList.add(getRandomIndex());
   }
-  componentDidMount() {
-    fetch(`${serverUrl}/amthal/random`)
-      .then((response) => response.json())
-      .then((json) => this.setState({ mathal: json }));
-  }
-  fetchAmthal() {
-    fetch(`${serverUrl}/amthal/ten`)
-      .then((response) => response.json())
-      .then((json) => this.setState({ amthal: json }));
-  }
-  render() {
-    return (
-      <div>
-        <h2>Today's Mathal</h2>
-        <p>{this.state.mathal.title}</p>
-        <hr />
-        <h3>More Amthal?</h3>
-        <button className="btn" onClick={this.fetchAmthal}>
-          Amthal
-        </button>
-        {
-            this.state.amthal.map(mathal =>{
-                return <p>  {mathal.title}</p>
-            })
-        }
-      </div>
-    );
-  }
-}
+  return Array.from(randomList).map(index => AMTHAL[index]);
+};
+
+const Amthal = () => {
+  const [mathal, setMathal] = useState({});
+  const [amthal, setAmthal] = useState([]);
+
+  useEffect(() => {
+    setMathal(randomMathal());
+  }, []);
+
+  const fetchAmthal = () => {
+    setAmthal(randomTen());
+  };
+
+  return (
+    <div>
+      <h2>Today's Mathal</h2>
+      <p>{mathal.title}</p>
+      <hr />
+      <h3>More Amthal?</h3>
+      <button className="btn" onClick={fetchAmthal}>
+        Amthal
+      </button>
+      {amthal.map((mathal, index) => (
+        <p key={index}>{mathal.title}</p>
+      ))}
+    </div>
+  );
+};
 
 export default Amthal;
